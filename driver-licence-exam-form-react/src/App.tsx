a55d72d2fe7bec2483f6fc4426ca77e539b7f446
emingineer <ahmettemin@gmail.com>
2019-04-19 21:47:37 +0300

    configuration

--- driver-licence-exam-form-react/src/App.tsx
+++ driver-licence-exam-form-react/src/App.tsx
@@ -8,16 +8,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <Login></Login>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     );
