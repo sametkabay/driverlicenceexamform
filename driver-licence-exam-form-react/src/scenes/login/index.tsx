f440c5ea9a5f6dff67a9449e012ef87fd65c79d5
emingineer <ahmettemin@gmail.com>
2019-04-19 22:41:12 +0300

    login page

--- driver-licence-exam-form-react/src/scenes/login/index.tsx
+++ driver-licence-exam-form-react/src/scenes/login/index.tsx
@@ -1,43 +1,22 @@
 import React from 'react';
-import './index.css'
+import './index.css';
+import AuthenticationStore from '../../store/authenticationStore'
 import {
     Container, Col, Form,
     FormGroup, Label, Input,
     Button,
 } from 'reactstrap';
+import LoginModel from '../../model/loginModel';
 
 class Login extends React.Component {
+
+    login = (username: string, password: string) => {
+        AuthenticationStore.login({ username, password } as LoginModel)
+    }
+
     render() {
         return (
-            <Container className="form-container">
-                <h2>Giriş Yap</h2>
-                <Form className="form">
-                    <Col>
-                        <FormGroup>
-                            <Label for="username">Kullanıcı Adı</Label>
-                            <Input
-                                style={{ width: '30%' }}
-                                name="username"
-                                id="username"
-                                placeholder="Kullanıcı Adı"
-                            />
-                        </FormGroup>
-                    </Col>
-                    <Col>
-                        <FormGroup>
-                            <Label for="password">Password</Label>
-                            <Input
-                                style={{ width: '30%' }}
-                                type="password"
-                                name="password"
-                                id="password"
-                                placeholder="********"
-                            />
-                        </FormGroup>
-                    </Col>
-                    <Button>Giriş</Button>
-                </Form>
-            </Container>
+            'Login Formu'
         );
     }
 }
